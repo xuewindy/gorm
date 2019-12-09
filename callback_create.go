35233739d6c6c4c935e8e6266a20c4efa70f9dad
sljeff <shanliangjeff@qq.com>
2019-12-09 11:31:18 +0800

    feat: update README

--- callback_create.go
+++ callback_create.go
@@ -49,128 +49,129 @@ func updateTimeStampForCreateCallback(scope *Scope) {
 
 // createCallback the callback used to insert data into database
 func createCallback(scope *Scope) {
-	if !scope.HasError() {
-		defer scope.trace(NowFunc())
+	if scope.HasError() {
+		return
+	}
+	defer scope.trace(NowFunc())
 
-		var (
-			columns, placeholders        []string
-			blankColumnsWithDefaultValue []string
-		)
+	var (
+		columns, placeholders        []string
+		blankColumnsWithDefaultValue []string
+	)
 
-		for _, field := range scope.Fields() {
-			if scope.changeableField(field) {
-				if field.IsNormal && !field.IsIgnored {
-					if field.IsBlank && field.HasDefaultValue {
-						blankColumnsWithDefaultValue = append(blankColumnsWithDefaultValue, scope.Quote(field.DBName))
-						scope.InstanceSet("gorm:blank_columns_with_default_value", blankColumnsWithDefaultValue)
-					} else if !field.IsPrimaryKey || !field.IsBlank {
-						columns = append(columns, scope.Quote(field.DBName))
-						placeholders = append(placeholders, scope.AddToVars(field.Field.Interface()))
-					}
-				} else if field.Relationship != nil && field.Relationship.Kind == "belongs_to" {
-					for _, foreignKey := range field.Relationship.ForeignDBNames {
-						if foreignField, ok := scope.FieldByName(foreignKey); ok && !scope.changeableField(foreignField) {
-							columns = append(columns, scope.Quote(foreignField.DBName))
-							placeholders = append(placeholders, scope.AddToVars(foreignField.Field.Interface()))
-						}
+	for _, field := range scope.Fields() {
+		if scope.changeableField(field) {
+			if field.IsNormal && !field.IsIgnored {
+				if field.IsBlank && field.HasDefaultValue {
+					blankColumnsWithDefaultValue = append(blankColumnsWithDefaultValue, scope.Quote(field.DBName))
+					scope.InstanceSet("gorm:blank_columns_with_default_value", blankColumnsWithDefaultValue)
+				} else if !field.IsPrimaryKey || !field.IsBlank {
+					columns = append(columns, scope.Quote(field.DBName))
+					placeholders = append(placeholders, scope.AddToVars(field.Field.Interface()))
+				}
+			} else if field.Relationship != nil && field.Relationship.Kind == "belongs_to" {
+				for _, foreignKey := range field.Relationship.ForeignDBNames {
+					if foreignField, ok := scope.FieldByName(foreignKey); ok && !scope.changeableField(foreignField) {
+						columns = append(columns, scope.Quote(foreignField.DBName))
+						placeholders = append(placeholders, scope.AddToVars(foreignField.Field.Interface()))
 					}
 				}
 			}
 		}
+	}
 
-		var (
-			returningColumn = "*"
-			quotedTableName = scope.QuotedTableName()
-			primaryField    = scope.PrimaryField()
-			extraOption     string
-			insertModifier  string
-		)
+	var (
+		returningColumn = "*"
+		quotedTableName = scope.QuotedTableName()
+		primaryField    = scope.PrimaryField()
+		extraOption     string
+		insertModifier  string
+	)
 
-		if str, ok := scope.Get("gorm:insert_option"); ok {
-			extraOption = fmt.Sprint(str)
+	if str, ok := scope.Get("gorm:insert_option"); ok {
+		extraOption = fmt.Sprint(str)
+	}
+	if str, ok := scope.Get("gorm:insert_modifier"); ok {
+		insertModifier = strings.ToUpper(fmt.Sprint(str))
+		if insertModifier == "INTO" {
+			insertModifier = ""
 		}
-		if str, ok := scope.Get("gorm:insert_modifier"); ok {
-			insertModifier = strings.ToUpper(fmt.Sprint(str))
-			if insertModifier == "INTO" {
-				insertModifier = ""
-			}
-		}
+	}
 
-		if primaryField != nil {
-			returningColumn = scope.Quote(primaryField.DBName)
-		}
+	if primaryField != nil {
+		returningColumn = scope.Quote(primaryField.DBName)
+	}
 
-		lastInsertIDOutputInterstitial := scope.Dialect().LastInsertIDOutputInterstitial(quotedTableName, returningColumn, columns)
-		var lastInsertIDReturningSuffix string
-		if lastInsertIDOutputInterstitial == "" {
-			lastInsertIDReturningSuffix = scope.Dialect().LastInsertIDReturningSuffix(quotedTableName, returningColumn)
-		}
+	lastInsertIDOutputInterstitial := scope.Dialect().LastInsertIDOutputInterstitial(quotedTableName, returningColumn, columns)
+	var lastInsertIDReturningSuffix string
+	if lastInsertIDOutputInterstitial == "" {
+		lastInsertIDReturningSuffix = scope.Dialect().LastInsertIDReturningSuffix(quotedTableName, returningColumn)
+	}
 
-		if len(columns) == 0 {
-			scope.Raw(fmt.Sprintf(
-				"INSERT%v INTO %v %v%v%v",
-				addExtraSpaceIfExist(insertModifier),
-				quotedTableName,
-				scope.Dialect().DefaultValueStr(),
-				addExtraSpaceIfExist(extraOption),
-				addExtraSpaceIfExist(lastInsertIDReturningSuffix),
-			))
-		} else {
-			scope.Raw(fmt.Sprintf(
-				"INSERT%v INTO %v (%v)%v VALUES (%v)%v%v",
-				addExtraSpaceIfExist(insertModifier),
-				scope.QuotedTableName(),
-				strings.Join(columns, ","),
-				addExtraSpaceIfExist(lastInsertIDOutputInterstitial),
-				strings.Join(placeholders, ","),
-				addExtraSpaceIfExist(extraOption),
-				addExtraSpaceIfExist(lastInsertIDReturningSuffix),
-			))
-		}
+	if len(columns) == 0 {
+		scope.Raw(fmt.Sprintf(
+			"INSERT%v INTO %v %v%v%v",
+			addExtraSpaceIfExist(insertModifier),
+			quotedTableName,
+			scope.Dialect().DefaultValueStr(),
+			addExtraSpaceIfExist(extraOption),
+			addExtraSpaceIfExist(lastInsertIDReturningSuffix),
+		))
+	} else {
+		scope.Raw(fmt.Sprintf(
+			"INSERT%v INTO %v (%v)%v VALUES (%v)%v%v",
+			addExtraSpaceIfExist(insertModifier),
+			scope.QuotedTableName(),
+			strings.Join(columns, ","),
+			addExtraSpaceIfExist(lastInsertIDOutputInterstitial),
+			strings.Join(placeholders, ","),
+			addExtraSpaceIfExist(extraOption),
+			addExtraSpaceIfExist(lastInsertIDReturningSuffix),
+		))
+	}
 
-		// execute create sql: no primaryField
-		if primaryField == nil {
-			if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
-				// set rows affected count
-				scope.db.RowsAffected, _ = result.RowsAffected()
+	// execute create sql: no primaryField
+	if primaryField == nil {
+		if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
+			// set rows affected count
+			scope.db.RowsAffected, _ = result.RowsAffected()
 
-				// set primary value to primary field
-				if primaryField != nil && primaryField.IsBlank {
-					if primaryValue, err := result.LastInsertId(); scope.Err(err) == nil {
-						scope.Err(primaryField.Set(primaryValue))
-					}
+			// set primary value to primary field
+			if primaryField != nil && primaryField.IsBlank {
+				if primaryValue, err := result.LastInsertId(); scope.Err(err) == nil {
+					scope.Err(primaryField.Set(primaryValue))
 				}
 			}
-			return
-		}
-
-		// execute create sql: lastInsertID implemention for majority of dialects
-		if lastInsertIDReturningSuffix == "" && lastInsertIDOutputInterstitial == "" {
-			if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
-				// set rows affected count
-				scope.db.RowsAffected, _ = result.RowsAffected()
-
-				// set primary value to primary field
-				if primaryField != nil && primaryField.IsBlank {
-					if primaryValue, err := result.LastInsertId(); scope.Err(err) == nil {
-						scope.Err(primaryField.Set(primaryValue))
-					}
-				}
-			}
-			return
-		}
-
-		// execute create sql: dialects with additional lastInsertID requirements (currently postgres & mssql)
-		if primaryField.Field.CanAddr() {
-			if err := scope.SQLDB().QueryRow(scope.SQL, scope.SQLVars...).Scan(primaryField.Field.Addr().Interface()); scope.Err(err) == nil {
-				primaryField.IsBlank = false
-				scope.db.RowsAffected = 1
-			}
-		} else {
-			scope.Err(ErrUnaddressable)
 		}
 		return
 	}
+
+	// execute create sql: lastInsertID implemention for majority of dialects
+	if lastInsertIDReturningSuffix == "" && lastInsertIDOutputInterstitial == "" {
+		if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
+			// set rows affected count
+			scope.db.RowsAffected, _ = result.RowsAffected()
+
+			// set primary value to primary field
+			if primaryField != nil && primaryField.IsBlank {
+				if primaryValue, err := result.LastInsertId(); scope.Err(err) == nil {
+					scope.Err(primaryField.Set(primaryValue))
+				}
+			}
+		}
+		return
+	}
+
+	// execute create sql: dialects with additional lastInsertID requirements (currently postgres & mssql)
+	if primaryField.Field.CanAddr() {
+		if err := scope.SQLDB().QueryRow(scope.SQL, scope.SQLVars...).Scan(primaryField.Field.Addr().Interface()); scope.Err(err) == nil {
+			primaryField.IsBlank = false
+			scope.db.RowsAffected = 1
+		}
+	} else {
+		scope.Err(ErrUnaddressable)
+	}
+	return
 }
 
 // forceReloadAfterCreateCallback will reload columns that having default value, and set it back to current object
